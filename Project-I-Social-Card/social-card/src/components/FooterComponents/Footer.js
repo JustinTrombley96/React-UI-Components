2d8152841f534341dcb931df2e72e5c3a025ed2e
Justin Trombley <jtrombley96@gmail.com>
2019-05-25 18:40:32 -0400

    React Finished

---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+	return (
+		<div className='footer'>
+			<i className='' />
+			<i className='' />
+			<i className='' />
+			<i className='' />
+		</div>
+	);
+};
+
+export default Footer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+	return (
+		<div className='header-title'>
+			<h4>Lambda School</h4>
+			<h5>@LambdaSchool . 26 jan</h5>
+		</div>
+	);
+};
+
+export default HeaderTitle;
